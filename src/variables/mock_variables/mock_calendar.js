ebf986d715b1a17ce5c2b0747d15d158ef22bc10
Patrick Aondona <aondopat@gmail.com>
2024-07-17 20:35:20 +0100

    Added mock items

---
+++ src/variables/mock_variables/mock_calendar.js
@@ -0,0 +1,38 @@
+export const calendarSample = [
+    {
+        name: "Mr. Amos Pikins",
+        image: "../../assets/images/Screenshot_2-removebg-preview 1.svg",
+        date: "26/06/2024",
+        time: "10:30 AM",
+        status: "confirmed"
+    },
+    {
+        name: "Mr. Adeyemi Eric",
+        image: "../../assets/images/Screenshot_2-removebg-preview 1.svg",
+        date: "26/05/2024",
+        time: "10:30 AM",
+        status: "confirmed"
+    },
+    {
+        name: "Mr. Osita D water",
+        image: "../../assets/images/Rectangle.svg",
+        date: "26/05/2024",
+        time: "10:30 AM",
+        status: "confirmed"
+    },
+    {
+        name: "Mrs. Juliet Nicki M",
+        image: "../../assets/images/Rectangle.svg",
+        date: "26/04/2024",
+        time: "10:30 AM",
+        status: "confirmed"
+    },
+    {
+        name: "Mr. Patrick Aondona",
+        image: "../../assets/images/Rectangle (1).svg",
+        date: "26/04/2024",
+        time: "10:30 AM",
+        status: "confirmed"
+    },
+]
+
