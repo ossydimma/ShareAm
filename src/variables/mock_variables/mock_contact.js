2d75ca83dbf82aef8f9f329624044948ef325536
Juliet-niki <nwajully@gmail.com>
2024-10-26 22:31:10 +0100

    first commit for contact new display

---
+++ src/variables/mock_variables/mock_contact.js
@@ -0,0 +1,136 @@
+export const contactSample = [
+  {
+    name: "Dr. Maria Watson",
+    image: "../../assets/images/Screenshot_2-removebg-preview 1.svg",
+    statusText: "Online",
+    status: "active",
+  },
+  {
+    name: "Mr. Sore Obudu",
+    image: "../../assets/images/user 1.png",
+    statusText: "Online",
+    status: "active",
+  },
+  {
+    name: "Prof. Amos Dgreat",
+    image: "",
+    statusText: "Away",
+    status: "away",
+  },
+  {
+    name: "Mary Chalton",
+    image: "../../assets/images/Rectangle.svg",
+    statusText: "Away",
+    status: "away",
+  },
+  {
+    name: "James Brown",
+    image: "",
+    statusText: "Online",
+    status: "active",
+  },
+  {
+    name: "Asiwaju Titi",
+    image: "../../assets/images/Rectangle (1).svg",
+    statusText: "Online",
+    status: "active",
+  },
+  {
+    name: "Alex Otti",
+    image: "",
+    statusText: "Away",
+    status: "away",
+  },
+  {
+    name: "Mr. Adeyemi Eric",
+    image: "../../assets/images/Rectangle (1).svg",
+    statusText: "Online",
+    status: "active",
+  },
+];
+
+export const blockedSample = [
+  {
+    name: "Dr. Maria Watson",
+    image: "../../assets/images/Screenshot_2-removebg-preview 1.svg",
+  },
+  {
+    name: "Mr. Sore Obudu",
+    image: "../../assets/images/user 1.png",
+  },
+  {
+    name: "Prof. Amos Dgreat",
+    image: "",
+  },
+  {
+    name: "Mary Chalton",
+    image: "../../assets/images/Rectangle.svg",
+  },
+  {
+    name: "James Brown",
+    image: "",
+  },
+  {
+    name: "Asiwaju Titi",
+    image: "../../assets/images/Rectangle (1).svg",
+  },
+  {
+    name: "Alex Otti",
+    image: "",
+  },
+  {
+    name: "Mr. Adeyemi Eric",
+    image: "../../assets/images/Rectangle (1).svg",
+  },
+];
+
+export const favouriteSample = [
+  {
+    name: "Dr. Maria Watson",
+    image: "../../assets/images/Screenshot_2-removebg-preview 1.svg",
+    statusText: "Online",
+    status: "active",
+  },
+  {
+    name: "Mr. Sore Obudu",
+    image: "../../assets/images/user 1.png",
+    statusText: "Online",
+    status: "active",
+  },
+  {
+    name: "Prof. Amos Dgreat",
+    image: "",
+    statusText: "Away",
+    status: "away",
+  },
+  {
+    name: "Mary Chalton",
+    image: "../../assets/images/Rectangle.svg",
+    statusText: "Away",
+    status: "away",
+  },
+  {
+    name: "James Brown",
+    image: "",
+    statusText: "Online",
+    status: "active",
+  },
+  {
+    name: "Asiwaju Titi",
+    image: "../../assets/images/Rectangle (1).svg",
+    statusText: "Online",
+    status: "active",
+  },
+  {
+    name: "Alex Otti",
+    image: "",
+    statusText: "Away",
+    status: "away",
+  },
+  {
+    name: "Mr. Adeyemi Eric",
+    image: "../../assets/images/Rectangle (1).svg",
+    statusText: "Online",
+    status: "active",
+  },
+];
