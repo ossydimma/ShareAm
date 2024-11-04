9282080ec1b4fae85f194241671a69be7e1e1623
Adeyemi Eric Ademola <119066166+Ericademola@users.noreply.github.com>
2024-11-05 00:21:52 +0100

    added feature to display various state for sidebar

--- src/ipcHandlers.js
+++ src/ipcHandlers.js
@@ -1,45 +1,51 @@
 const { BrowserWindow, ipcMain, nativeTheme } = require('electron');
 function registerIPCMainHandlers() {
 
-    ipcMain.handle('dark-mode:toggle', () => {
-        nativeTheme.themeSource = nativeTheme.shouldUseDarkColors ? 'light' : 'dark';
-        return nativeTheme.shouldUseDarkColors;
-      });
-    
-      ipcMain.handle('dark-mode:system', () => {
-        nativeTheme.themeSource = 'system';
-      });
-    
-    
-    ipcMain.handle('minimize-window', async () => {
-      const focusedWindow = BrowserWindow.getFocusedWindow();
-      if (focusedWindow) {
-        focusedWindow.minimize();
-        return true; // Return true to indicate success or any other relevant data
+  ipcMain.handle('dark-mode:toggle', () => {
+    nativeTheme.themeSource = nativeTheme.shouldUseDarkColors ? 'light' : 'dark';
+    return nativeTheme.shouldUseDarkColors;
+  });
+
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system';
+  });
+
+
+  ipcMain.handle('minimize-window', async () => {
+    const focusedWindow = BrowserWindow.getFocusedWindow();
+    if (focusedWindow) {
+      focusedWindow.minimize();
+      return true; // Return true to indicate success or any other relevant data
+    }
+    return false; // Return false or any error indication if there is no focused window
+  });
+
+  ipcMain.handle('maximize-window', async () => {
+    const focusedWindow = BrowserWindow.getFocusedWindow();
+    if (focusedWindow) {
+      if (focusedWindow.isMaximized()) {
+        focusedWindow.unmaximize();
+      } else {
+        focusedWindow.maximize();
       }
-      return false; // Return false or any error indication if there is no focused window
-    });
-    
-    ipcMain.handle('maximize-window', async () => {
-      const focusedWindow = BrowserWindow.getFocusedWindow();
-      if (focusedWindow) {
-        if (focusedWindow.isMaximized()) {
-          focusedWindow.unmaximize();
-        } else {
-          focusedWindow.maximize();
-        }
-        return true; // Return true to indicate success or any other relevant data
-      }
-      return false; // Return false or any error indication if there is no focused window
-    });
-    
-    ipcMain.handle('close-window', async () => {
-      const focusedWindow = BrowserWindow.getFocusedWindow();
-      if (focusedWindow) {
-        focusedWindow.close();
-        return true; // Return true to indicate success or any other relevant data
-      }
-      return false; // Return false or any error indication if there is no focused window
-    });
+      return true; // Return true to indicate success or any other relevant data
+    }
+    return false; // Return false or any error indication if there is no focused window
+  });
+
+  ipcMain.handle('close-window', async () => {
+    const focusedWindow = BrowserWindow.getFocusedWindow();
+    if (focusedWindow) {
+      focusedWindow.close();
+      return true; // Return true to indicate success or any other relevant data
+    }
+    return false; // Return false or any error indication if there is no focused window
+  });
+
+  ipcMain.handle('show-value:toggle', async (event, newValue) => {
+    console.log('New value:', newValue);
+    return newValue;
+  });
+
 }
 module.exports = { registerIPCMainHandlers };
